2abf6997486b9443d39a942f2c07c43f247a7732
Myris <myriszadip@gmail.com>
2017-12-29 19:54:13 +0100

    add entity classroom

---
+++ src/ClassBundle/Entity/Classroom.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace ClassBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Classroom
+ *
+ * @ORM\Table(name="classroom")
+ * @ORM\Entity(repositoryClass="ClassBundle\Repository\ClassroomRepository")
+ */
+class Classroom
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="seat", type="integer")
+     */
+    private $seat;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="speaker", type="string", length=255)
+     */
+    private $speaker;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Classroom
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set seat
+     *
+     * @param integer $seat
+     *
+     * @return Classroom
+     */
+    public function setSeat($seat)
+    {
+        $this->seat = $seat;
+
+        return $this;
+    }
+
+    /**
+     * Get seat
+     *
+     * @return int
+     */
+    public function getSeat()
+    {
+        return $this->seat;
+    }
+
+    /**
+     * Set speaker
+     *
+     * @param string $speaker
+     *
+     * @return Classroom
+     */
+    public function setSpeaker($speaker)
+    {
+        $this->speaker = $speaker;
+
+        return $this;
+    }
+
+    /**
+     * Get speaker
+     *
+     * @return string
+     */
+    public function getSpeaker()
+    {
+        return $this->speaker;
+    }
+}
+
